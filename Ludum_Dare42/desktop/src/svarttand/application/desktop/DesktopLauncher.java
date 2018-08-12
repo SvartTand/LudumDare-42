c827134676e6f352f54321c779d99224cbf5da35
Blackstorm <Blackstorm@86.50.78.77>
2018-08-13 00:12:28 +0300

    Icon and name

--- Ludum_Dare42/desktop/src/svarttand/application/desktop/DesktopLauncher.java
+++ Ludum_Dare42/desktop/src/svarttand/application/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package svarttand.application.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import svarttand.application.Application;
@@ -9,6 +10,8 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 800;
+		config.title = "The Screen Saver";
+		config.addIcon("Icon.png", FileType.Internal);
 		new LwjglApplication(new Application(), config);
 	}
 }
